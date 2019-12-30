4d1429ef04d63660d8ade1a8f54d14dcd548f01b
Angelo Shema <geoangercola@gmail.com>
2019-12-30 05:16:48 +0200

    Prepared for push

--- js/bundle.js
+++ js/bundle.js
@@ -82,6 +82,50 @@ let V = {
 	init: function() {
 		V.menu.registerAll();
 		V.dialog.registerAll();
+		V.notifs.registerAll();
+	},
+	notifs: {
+		new: function(value = "Working", type = "default", wait = 5000) {
+			notif = document.createElement("div");
+			notif.classList.add("notif-float");
+			notif.classList.add("notif");
+			notif.classList.add("notif-" + type);
+			if (!value.startsWith("<")) {
+				value = "<span>"+ value + "</span>"
+			}
+			notif.innerHTML = value;
+			document.body.appendChild(notif);
+			V.notifs.register(notif, wait);
+			setTimeout(function() {
+				V.notifs.close(notif);
+			}, wait);
+			return notif;
+		},
+		close: function(notif, wait = 0) {
+			if (typeof notif == "undefined") {
+				return 1
+			}
+			setTimeout(function() {
+				notif.setAttribute("closed", "");
+			}, wait + 300);
+			setTimeout(function() {
+				notif.hidden = true;
+			}, wait);
+		},
+		registerAll: function() {
+			notifs = document.getElementsByClassName("notif");
+			for (var i = 0; i < notifs.length; i++) {
+				V.notifs.register(notifs[i]);
+			}
+		},
+		register: function(notif, wait = Infinity) {
+			if (typeof notif == "undefined") {
+				return 1
+			}
+			notif.onclick = function() {
+				V.notifs.close(notif);
+			}
+		}
 	}
 }
 document.addEventListener("load", V.init());
